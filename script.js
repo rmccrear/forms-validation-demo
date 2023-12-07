a215e34c4be5ab142a2a683fc083853f7c6147cf
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-12-06 17:37:22 -0800

    Added some validation with for loops.
    Co-authored-by: Laqwanda Nettles <Laqwanda-Nettles@users.noreply.github.com>
    Co-authored-by: djembeman2 <djembeman2@users.noreply.github.com>
    Co-authored-by: Jordan W. Toujouse <jordan2juice@users.noreply.github.com>
    Co-authored-by: KH-Ward <KH-Ward@users.noreply.github.com>
    Co-authored-by: DvFaucheux <DvFaucheux@users.noreply.github.com>
    Co-authored-by: Isabella Saenz <IsabellaSAENZ@users.noreply.github.com>

--- script.js
+++ script.js
@@ -9,15 +9,77 @@ console.log("hello");
 function validate() {
     let inputs;
     inputs = document.querySelectorAll("form input");
-    for (let elm of inputs) {
-        if (elm.value === "") {
-            elm.style.border = "solid red";
+    for (let input of inputs) {
+        if (input.value === "") {
+            input.style.border = "solid red";
         } else {
-            elm.style.border = "solid green";
+            input.style.border = "solid green";
         }
     }
-}
 
+    // detect if the input has a & or * character in it.
+     for (let input of inputs) {
+        for(let alpha of input.value) {
+            if(alpha === '&') {
+                input.style.color = "gold";
+            } else if(alpha === '*') {
+                input.style.color = "gold";
+            } else {
+                input.style.color = "black";
+            }
+        }
+    }
+
+    // detect if a VIP name is in the inputs
+
+    for (let input of inputs) {
+        let vips = [
+            "Robert", "Lynndale", "Laqwanda", "Isabella", "Jeremie", "Delta", "Jordan", "Kenyada"
+        ];
+        for(let name of vips){
+            if(input.value.toLowerCase() === name.toLowerCase()) {
+                input.style.color = "goldenrod";
+            }
+        }
+    }
+
+    // for (let input of inputs){
+    //     if () {
+            
+    //     }
+    // }
+    // for (let input of inputs) {
+    //     if(){
+            
+    //     }
+    // }
+    
+    for(let input of inputs) {
+        if(input.value=== "lynndale") 
+        input.style.border="light green"}
+    
+    
+    for(let input of inputs) {
+      if(input.value === "abcd") {
+        input.style.border="solid purple"      
+      }   
+    }
+
+    for(let input of inputs){
+        input.style.border = "solid " + input.value;
+    }
+    
+    for(let input of inputs){
+       if(input.value === "Robert") {
+        input.style.border="solid cornflowerblue";
+       }    
+    }
+    for(let input of inputs) {
+        if(input.value === "Delta") {
+            input.style.border = "solid blue"
+        }
+    }
+
+}    
 let button = document.querySelector('#my-button');
 button.onclick = validate;
-
